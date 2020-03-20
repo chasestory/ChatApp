ff78484941fd231cb232f477609c21092a328737
Chase Story <chasestory@outlook.com>
2020-03-19 21:10:19 -0600

    added notifications to browser, for new messages

--- app/jobs/message_relay_job.rb
+++ app/jobs/message_relay_job.rb
@@ -3,7 +3,9 @@ class MessageRelayJob < ApplicationJob
 
   def perform(message)
     ActionCable.server.broadcast "chatrooms:#{message.chatroom.id}", {
-      message: MessagesController.render(message),
+      # message: MessagesController.render(message),
+      body: message.body,
+      username: message.user.username,
       chatroom_id: message.chatroom.id
     }
   end
