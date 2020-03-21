e1e3baf40627842d205d7ee0858808b7a3040d1d
Chase Story <chasestory@outlook.com>
2020-03-21 15:35:49 -0600

    added direct messages setup

--- app/controllers/direct_messages_controller.rb
+++ app/controllers/direct_messages_controller.rb
@@ -3,7 +3,7 @@ before_action :authenticate_user!
 
 	def show
 		users = [current_user, User.find(params[:id])]
-	@chatroom = Chatroom.direct_messages_for_users([current_user, User.find(params[:id])])
+	@chatroom = Chatroom.direct_messages_for_users([current_user, User.find(users)
 	end
 
 
--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -3,20 +3,20 @@ class Chatroom < ApplicationRecord
 	has_many :users, through: :chatroom_users
 	has_many :messages
 
-	scope :public, -> { where(direct_message: false) }
-	scope :direct_message, -> { where(direct_message: true)}
+	scope :public_channel, -> { where(direct_message: false) }
+	scope :direct_messages, -> { where(direct_message: true)}
 
 	def self.direct_message_for_users(users)
 		user_ids = users.map(&:id).sort
 		name = "DM:#{user_ids.join(":")}"
 		
-		if chatroom = direct_message.where(name: name)
-			return chatroom
+		if chatroom = direct_messages.where(name: name).first
+			chatroom
 		else
 			chatroom = new(name: name, direct_message: true)
 			chatroom.users = users
 			chatroom
 		end
 	end
-	
+
 end
