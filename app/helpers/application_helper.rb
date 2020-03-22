3079cd98af6217af3b7ac1410517ff795882c018
Chase Story <chasestory@outlook.com>
2020-03-22 00:57:12 -0600

    added active styles for dropdown, added updated styleds

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def active_class(link_path)
+		request.fullpath == link_path ? "active" : "" 
+	end
+
+	# def active_class(link_path)
+	# 	current_page?(link_path) ? "active" : ""
+	#  end
+
 end
